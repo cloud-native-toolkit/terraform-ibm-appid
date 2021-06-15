31bfffecddeb2f1a43027a956e0de1aa56a40fa4
Sean Sundberg <seansund@us.ibm.com>
2021-06-15 07:32:27 -0500

    Fix appid module as per other modules (#6)
    * Update module.yaml
    * Fix provider versions
    * Update variables
    
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>
    Co-authored-by: Mihir Shah <mrshah@us.ibm.com>

---
+++ provider.tf
@@ -0,0 +1,4 @@
+provider "ibm" {
+  region           = var.resource_location
+  ibmcloud_api_key = var.ibmcloud_api_key
+}
--- variables.tf
+++ variables.tf
@@ -1,3 +1,8 @@
+variable "ibmcloud_api_key" {
+  type        = string
+  description = "The api key for IBM Cloud access"
+}
+
 variable "resource_group_name" {
   type        = string
   description = "Resource group where the cluster has been provisioned."
